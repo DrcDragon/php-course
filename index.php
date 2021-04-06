373fd4145cd06ecb9c1c965c05dbd196bad43ba8
David Cook <davidrichardcook@hotmail.co.uk>
2021-04-06 11:16:05 +0100

    add tax calc function

--- index.php
+++ index.php
@@ -15,23 +15,39 @@
     echo "<h1>Welcome to ".$name."!</h1>";
 	echo "<h2>You have $".$credit." in your wallet.</h2>";
 	
-	//Array of Products
+		//Array of Products
 		$products['Computer']=750;
 		$products['Car']=15000;
 		$products['iPhone']=1000;
 		$products['Toaster']=75;
-		
+	
+		//foreach loop to list all products in the array	
 		foreach($products as $key => $value){		
 			echo "<p>The ".$key." costs $".$value,"</p>";
 		}
 		
 		echo "<h2>Items you can afford</h2>";
-		
+		//foreach loop with a conditional stamement to list any items in the array that is less or equal to the varible $credit	
 		foreach($products as $key => $value){
 			if($value <= $credit ){
 				echo "<p>".$key."</p>";
 			}
 		}
+		
+		// Using Maths to the sum of 2 varibles
+		$amount=800;
+		$taxRate=0.0825;
+		$addedTax = $amount*$taxRate;	//amount = 800, tax = .0825
+		echo $addedTax;
+		
+		//Create a Function
+		function tax_calc($amount,$tax){
+			$calculate_tax = $amount*$tax;
+			$amount = round($amount+$calculate_tax,2);
+			return $amount;
+		}
+		
+		echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
